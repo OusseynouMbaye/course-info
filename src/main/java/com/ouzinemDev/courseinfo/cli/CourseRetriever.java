be69a86307cea18ffeb5e953752ceb39ca18ffd0
Ouzinem <ouzinem@hotmail.com>
2022-10-21 11:01:41 -0400

    Add slf4j dependency injection

--- src/main/java/com/ouzinemDev/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/ouzinemDev/courseinfo/cli/CourseRetriever.java
@@ -1,22 +1,27 @@
 package com.ouzinemDev.courseinfo.cli;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CourseRetriever {
+    private static final Logger LOG =
+            LoggerFactory.getLogger(CourseRetriever.class);
+
     public static void main(String[] args) {
-        out("CourseRetriever started");
+        LOG.info("CourseRetriever started");
         if (args.length == 0) {
-            out("Please provide an author name as first argument");
+            LOG.warn("Please provide an author name as first argument");
             return;
         }
-      try {
-          retrieveCourses(args[0]);
-      }catch (Exception e){
-          out("Unexpected error");
-          e.printStackTrace();
-      }
+        try {
+            retrieveCourses(args[0]);
+        } catch (Exception e) {
+           LOG.error("Unexpected error",e);
+        }
     }
 
     private static void retrieveCourses(String authorId) {
-        out("Retrieving course for author " + authorId);
+        LOG.info("Retrieving course for author '{}'" ,authorId);
     }
 
     public static void out(Object o) {
