e76be255574e357b0fe6ea069b28a8d6edd86187
Ouzinem <ouzinem@hotmail.com>
2022-10-23 07:22:20 -0400

    Using Java's HttpClient for call API

--- src/main/java/com/ouzinemDev/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/ouzinemDev/courseinfo/cli/CourseRetriever.java
@@ -1,5 +1,6 @@
 package com.ouzinemDev.courseinfo.cli;
 
+import com.ouzinemDev.courseinfo.cli.service.CourseRetrievalService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,12 +17,16 @@ public class CourseRetriever {
         try {
             retrieveCourses(args[0]);
         } catch (Exception e) {
-           LOG.error("Unexpected error",e);
+            LOG.error("Unexpected error", e);
         }
     }
 
     private static void retrieveCourses(String authorId) {
-        LOG.info("Retrieving course for author '{}'" ,authorId);
+        LOG.info("Retrieving course for author '{}'", authorId);
+        CourseRetrievalService courseRetrievalService = new CourseRetrievalService();
+
+        String coursesToStore = courseRetrievalService.getCoursesFor(authorId);
+        LOG.info("Retrieved the following courses {}", coursesToStore);
     }
 
     public static void out(Object o) {
